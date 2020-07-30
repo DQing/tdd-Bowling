23c71a7645667be5218670f375d9fb9aa7d17cd8
QingQing Dou <qqdou@thoughtworks.com>
2020-07-30 21:27:51 +0800

    should return 60 if every roll is 3
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,9 +1,13 @@
-public class BowlingGame {
-    public void roll(int pouredNumber) {
+import java.util.ArrayList;
 
+public class BowlingGame {
+    ArrayList<Integer> pouredNumbers = new ArrayList<>();
+
+    public void roll(int pouredNumber) {
+        pouredNumbers.add(pouredNumber);
     }
 
     public int score() {
-        return 0;
+        return pouredNumbers.stream().mapToInt(num -> num).sum();
     }
 }
--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -1,16 +1,36 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setup() {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
     public void should_return_0_when_scoring_given_every_roll_is_0() {
-        BowlingGame bowlingGame = new BowlingGame();
-        for (int rollIndex = 0; rollIndex < 20; rollIndex++) {
-            bowlingGame.roll(0);
-        }
+        initBowlingGamePouredNum(0);
 
         int score = bowlingGame.score();
 
         Assert.assertEquals(0, score);
+    }
+
+    @Test
+    public void should_return_correct_score_when_scoring_given_every_roll_is_3() {
+        initBowlingGamePouredNum(3);
+
+        int score = bowlingGame.score();
+
+        Assert.assertEquals(60, score);
+    }
+
+    private void initBowlingGamePouredNum(int pouredNumber) {
+        for (int rollIndex = 0; rollIndex < 20; rollIndex++) {
+            bowlingGame.roll(pouredNumber);
+        }
     }
 }
